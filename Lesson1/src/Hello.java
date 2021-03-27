60cd48c1a18124406adc7fc6cb3bb3885e6b0b1d
arsent <arsentevanyan@gmail.com>
2021-03-27 16:44:45 +0400

    first commit

---
+++ Lesson1/src/Hello.java
@@ -0,0 +1,45 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello PicsArt");
+
+        byte a = 4;            // 8 bit
+        short b = 14544;       // 16 bit
+        int i = 646544;        // 32 bit
+        long l = 5465656566L;  //64 bit
+
+        float f = 45.32F;      // 32 bit
+        double d = 12.65;      // 64 bit
+
+        boolean bb = false;     //  true or false
+
+        char ch = 'e';          // symbol or unicode
+        char chu = '\u0025';
+
+        String s = "inch uzem karam grem";
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bb);
+        System.out.println(ch);
+        System.out.println(chu);
+        System.out.println(s);
+        System.out.println("a = " + a + ", b = " + b);
+    }
+
+}
+
+
+//CTRL  +  ALT +  L  = sirunacnuma
+//CTRL  +  SPACE  = hint bar
+//CTRL  +  D  =  duplicate row
+//ALT  +  ENTER
+
+//git commit
+//git push
