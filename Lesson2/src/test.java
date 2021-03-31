36a6b275710eb6e33b9483ba00dd1061c92dbd3d
arsent <arsentevanyan@gmail.com>
2021-03-31 15:00:48 +0400

    first1

--- Lesson2/src/test.java
+++ Lesson2/src/test.java
@@ -15,5 +15,15 @@ public class test {
         System.out.println((a > b) && (a < b));
         System.out.println((a > b) || (a < b));
         System.out.println(!(a > b) && (a < b));
+
+        int c = 60, d = 13;
+        System.out.println(c & d);
+        System.out.println(c | d);
+        System.out.println(c ^ d);
+        System.out.println(c >>> 2);
+        System.out.println(~c);
+        System.out.println(d << 1);
+        c += d;
+
     }
 }
