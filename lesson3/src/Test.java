9f463a0249a7cbd73faa51e24341367f822c0a34
arsent <arsentevanyan@gmail.com>
2021-04-14 20:07:05 +0400

    1

---
+++ lesson3/src/Test.java
@@ -0,0 +1,29 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 5;
+
+        for (int i = 1; i <= x; i++) {
+            for (int k = 0; k < i; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
