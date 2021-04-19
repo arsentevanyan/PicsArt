aaae81826b71e9c892ece2c9e707b0e77ef056d4
arsent <arsentevanyan@gmail.com>
2021-04-19 13:13:03 +0400

    1 add isPolindrom1h addd fibonachi and ..

--- Lesson6/src/Test.java
+++ Lesson6/src/Test.java
@@ -6,6 +6,22 @@ public class Test {
 
         Test t = new Test();
         System.out.println(t.isPalindrome(s));
+        Test fib =new Test();
+        System.out.println(fib.fib(8));
+        Test tt =new Test();
+        System.out.println(tt.f(7));
+
+
+
+        int y = 5;
+
+        for (int i = 1; i <= y; i++) {
+            for (int k = 0; k < i; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+
     }
 
     public boolean isPalindrome(String s) {
@@ -19,4 +35,24 @@ public class Test {
         return true;
 
     }
+    public int f(int x) {
+        if (x == 0 || x == 1) {
+            return 1;
+        }
+        return x * f(x - 1);
+
+
+    }
+
+    public int fib(int xx) {
+        if (xx == 1 || xx == 2) {
+            return 1;
+        }
+        return fib(xx - 1) + fib(xx - 2);
+
+
+    }
+
+
+
 }
