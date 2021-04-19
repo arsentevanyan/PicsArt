da50170a995baee9aa3a24dc5ddd7fd18da9b216
arsent <arsentevanyan@gmail.com>
2021-04-19 12:50:34 +0400

    first2

--- Lesson7/src/main/Test.java
+++ Lesson7/src/main/Test.java
@@ -4,6 +4,7 @@ import model.Human;
 import service.HumanService;
 
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class Test {
 
@@ -20,46 +21,72 @@ public class Test {
         human1.printInfo();
         human2.printInfo();
         human3.printInfo();
+        Human[] humans = {human1, human2, human3};
 
-        Human[] humans;
-        humans = new Human[]{human1, human2, human3};
 
-        System.out.println("------------------------------");
-        System.out.println("Is human birth after 2000 year");
-        service.printYesYearAfter2000(human1);
-        System.out.println("------------------------------");
-        System.out.println("Name of heavier human of 2");
-        System.out.println(service.nameOfHeavier(human1, human2));
-        System.out.println("------------------------------");
-        System.out.println("Print longest last name human of 2");
-        service.humanEithLongestLastName(human1, human2).printInfo();
-        System.out.println("------------------------------");
-        System.out.println("Print youngest human name of 3");
-        service.printYoungestName(human1, human2, human3);
-        System.out.println("------------------------------");
-        System.out.println("Print full name of Armenian after 2000 ( arrey 3 )");
-        service.printFullNameOfArmenianAfter2000(humans);
-        System.out.println("------------------------------");
-        System.out.println("Print smallest weight ( arrey 3 )");
-        System.out.println(service.smallestWeight(humans).getWeight());
-        ;
-        System.out.println("------------------------------");
-        System.out.println("print count of not Armenian ( arrey 3 )");
-        service.countOfNotArmenians(humans);
-        System.out.println("------------------------------");
-        System.out.println("Print humans after 2000 (array 3 )");
-        for (int i = 0; i < service.humansAfter2000(humans).length; i++) {
-            service.humansAfter2000(humans)[i].printInfo();
+        Scanner s = new Scanner(System.in);
+
+        boolean isMenuActive = true;
+        while (isMenuActive) {
+
+            System.out.println("Enter command number");
+            System.out.println("1.  Is human birth after 2000 year");
+            System.out.println("2.  Name of heavier human of 2");
+            System.out.println("3.  Print longest last name human of 2");
+            System.out.println("4.  Print youngest human name of 3");
+            System.out.println("5.  Print full name of Armenian after 2000 ( arrey 3 )");
+            System.out.println("6.  Print smallest weight ( arrey 3 )");
+            System.out.println("7.  print count of not Armenian ( arrey 3 )");
+            System.out.println("8.  Print humans after 2000 (array 3 )");
+            System.out.println("9.  Print oldest arminean ( arrey 3 )");
+            System.out.println("10. Print humans ordered by weight");
+            System.out.println("11. Exit");
+
+            int comand = s.nextInt();
+
+            switch (comand) {
+
+                case 1:
+                    service.printYesYearAfter2000(human1);
+                    break;
+                case 2:
+                    System.out.println(service.nameOfHeavier(human1, human2));
+                    break;
+                case 3:
+                    service.humanEithLongestLastName(human1, human2).printInfo();
+                    break;
+                case 4:
+                    service.printYoungestName(human1, human2, human3);
+                    break;
+                case 5:
+                    service.printFullNameOfArmenianAfter2000(humans);
+                    break;
+                case 6:
+                    System.out.println(service.smallestWeight(humans).getWeight());
+                    break;
+                case 7:
+                    service.countOfNotArmenians(humans);
+                    break;
+                case 8:
+                    for (int i = 0; i < service.humansAfter2000(humans).length; i++) {
+                        service.humansAfter2000(humans)[i].printInfo();
+                    }
+                    break;
+                case 9:
+                    service.oldestFromArmenians(humans).printInfo();
+                    break;
+                case 10:
+                    for (int i = 0; i < service.orderbyWeight(humans).length; i++) {
+                        service.orderbyWeight(humans)[i].fullName();
+                    }
+                    break;
+                case 11:
+                    isMenuActive = false;
+                    System.out.println("bye");
+                    break;
+
+            }
         }
-        System.out.println("------------------------------");
-        System.out.println("Print oldest arminean ( arrey 3 )");
-        service.oldestFromArmenians(humans).printInfo();
-        System.out.println("------------------------------");
-        System.out.println("Print humans ordered by weight");
-        for (int i = 0; i < service.humansAfter2000(humans).length; i++) {
-            service.humansAfter2000(humans)[i].fullName();
-        }
-        System.out.println("------------------------------");
 
 
     }
